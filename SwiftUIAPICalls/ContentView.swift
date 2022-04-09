1b0531ac3a8f52d9918a2c67ff3d5b12557ea148
jinlidavid <jin_li.d@etoncollege.org.uk>
2022-04-09 16:26:13 +0100

    Initial Commit

---
+++ SwiftUIAPICalls/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAPICalls
+//
+//  Created by David Jin Li on 09/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAPICalls/SwiftUIAPICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPICallsApp.swift
+//  SwiftUIAPICalls
+//
+//  Created by David Jin Li on 09/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
